ae0dde939b38a26e39a5cee7ec0cdf4d27a80b1b
fengjie <mars.fengjie@gmail.com>
2017-11-22 09:47:35 +0800

    Update export

--- build/index.js
+++ build/index.js
@@ -6,3 +6,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./zipkin/GrpcImpl"));
 __export(require("./zipkin/KoaImpl"));
 __export(require("./zipkin/TypeOrmImpl"));
+var ZipkinBase_1 = require("./zipkin/abstract/ZipkinBase");
+exports.ZIPKIN_EVENT = ZipkinBase_1.ZIPKIN_EVENT;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './zipkin/GrpcImpl';
 export * from './zipkin/KoaImpl';
 export * from './zipkin/TypeOrmImpl';
+export {ZIPKIN_EVENT} from './zipkin/abstract/ZipkinBase';
